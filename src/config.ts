cfb7a4c69d91cc079aafccd178f7b9b0d9a9a994
Bijoy Lohar <loharbijoy2005@gmail.com>
2026-08-12 22:55:11 +0530

    Update live WhatsApp Access Token & MongoDB Atlas configuration

--- src/config.ts
+++ src/config.ts
@@ -4,4 +4,4 @@ export const RAZORPAY_KEY_ID = "rzp_live_TKUawOJDuTeooz"; // Razorpay API Key ID
 
 // 2. Meta WhatsApp Business API Credentials for OTP SMS
 export const WHATSAPP_PHONE_NUMBER_ID = "1318722734646618"; // Meta WhatsApp Phone Number ID
-export const WHATSAPP_ACCESS_TOKEN = "";    // Insert Meta WhatsApp Access Token (starts with EAA...)
+export const WHATSAPP_ACCESS_TOKEN = "EAAM8xGxJ5ZCgBSKrLmVTfIxmt2J9HE7QvvBMOAPZBpF5QbsVhkTGAzSwEfRAkmIm3xSZAs57aBSmQxZBdaUZBAWCIyrpjEqfGp93PaZCsOt5xfVzZBynIbf1mDpuNp0rvL1d0GohJX2mst6plp7BovXXhqv7JEZAHb1U2owePdv6qfsxKfJ3ApZABeYpUYut0ZAZBuqudubQf5Tqhxq7WqME553HwKZAo7PJWZAEXC41ZAaMdZAmsPmvr5hRvJlbqtjqvv2UuejqRDSdZAzLpspLWhgPYPXmZAAbweQUBcO6B1tePWgZDZD";
